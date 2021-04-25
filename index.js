9856f1ff4847ab26407ed88c94857bffcf5b6d84
CVA1826 <79380824+CVA1826@users.noreply.github.com>
2021-04-25 00:15:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("No creo que leas esto, pero por si lo lees...")
+console.log("Enserio te amo demasiado y sabes de que manera")
+console.log("Te necesito en mi vida")
+console.log("muaaackkkk")
+
+alert("Te amoooo")
+alert("Callate y besame.")
+alert("digo digo..., nah a la vrga, sí quiero un beso tuyo.")
